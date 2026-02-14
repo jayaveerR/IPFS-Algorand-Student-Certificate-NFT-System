71ef4a8a5cd6df5f9320489acaabd1d7a7837a07
jayaveerR <ramanadhamjayaveer@gmail.com>
2026-02-14 08:53:39 +0530

    APP

--- projects/backend-frontend/src/components/AppCalls.tsx
+++ projects/backend-frontend/src/components/AppCalls.tsx
@@ -54,6 +54,7 @@ const AppCalls = ({ openModal, setModalState }: AppCallsInterface) => {
 
     const { appClient } = deployResult
 
+    /*
     const response = await appClient.send.hello({ args: { name: contractInput } }).catch((e: Error) => {
       enqueueSnackbar(`Error calling the contract: ${e.message}`, { variant: 'error' })
       setLoading(false)
@@ -65,6 +66,7 @@ const AppCalls = ({ openModal, setModalState }: AppCallsInterface) => {
     }
 
     enqueueSnackbar(`Response from the contract: ${response.return}`, { variant: 'success' })
+    */
     setLoading(false)
   }
 
